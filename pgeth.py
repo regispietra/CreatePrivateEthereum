5a4f0f06d37b2e8f73139bf55307b8e7c572eea3
régis pietraszewski <rpietra@gmail.com>
2016-06-16 16:41:06 +0200

    first commit

--- pgeth.py
+++ pgeth.py
@@ -3,19 +3,23 @@
 
 import sys
 import argparse
+import subprocess
 
 def init(args):
+    """doc3S"""
+    subprocess.call(["geth", "--networkid 100 --identity node1 --verbosity 3 --nodiscover --nat none init"])
     print args
 
 def start(args):
+    """ doc """
     print args
 
 def stop(args):
+    """ doc """
     print args
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'to be completed')
-    args = parser.parse_args()
 
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
